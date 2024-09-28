11a1f8fcfa2fb0073419f22e21fac9cbb7d750f2
patromi <patrom123@gmail.com>
2024-09-28 14:25:14 +0200

    stt/tts

--- projects/backend/taxHackYeah/taxHackYeah/settings.py
+++ projects/backend/taxHackYeah/taxHackYeah/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'taxHackYeah.apps.TaxhackyeahConfig',
+    'api',
+    'chatsocket',
     'rest_framework.authtoken',
 
 ]
@@ -79,11 +80,14 @@ WSGI_APPLICATION = 'taxHackYeah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',              # Nazwa bazy danych, tak jak w docker-compose
+        'USER': 'admin',              # Użytkownik PostgreSQL
+        'PASSWORD': 'admin',          # Hasło użytkownika
+        'HOST': 'postgres',           # Nazwa usługi PostgreSQL z docker-compose
+        'PORT': '5432',               # Port PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
