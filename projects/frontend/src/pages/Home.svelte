53ca5817961cee2aeef9376f61e8fc3d6f6a467e
Dominik <dominik.malkowiak@gmail.com>
2024-09-29 11:46:39 +0200

    15 minutes left..

--- projects/frontend/src/pages/Home.svelte
+++ projects/frontend/src/pages/Home.svelte
@@ -9,8 +9,11 @@ let error = $state();
 onMount(() => {
   messageStore.subscribe(msg => {
     if (msg.connected === false) {
-      // TODO: ENABLE
-      // error = true;
+      //error = true;
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 500);
     }
   });
 });
@@ -28,7 +31,7 @@ onMount(() => {
   </div>
 
   <div class="tutorial">
-    <h2>Tutorial</h2>
+    <h2>Poradnik</h2>
 
     <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
 
